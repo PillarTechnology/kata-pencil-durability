9ba38fa8b6fdaf3864706a8826c20826feb8992b
kPOWz <karrie.zeman@gmail.com>
2018-10-16 21:51:51 -0500

    adds unit testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render, waitForElement} from 'react-testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the heading', () => {
+  const div = document.createElement('div');
+  const {container, getByText} = render(<App />, div);
+
+  expect(container.querySelector('h1')).toBeInTheDocument();
+  expect(getByText('Pencil Durability'));
 });
