2323d32aaa1ce1b74a57b2649e094cb3ea200221
kPOWz <karrie.zeman@gmail.com>
2018-10-16 22:22:18 -0500

    proves HTML inputs work >.< for writing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {render, waitForElement} from 'react-testing-library';
+import getlorem from 'getlorem';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -19,10 +20,23 @@ it('renders the heading', () => {
 
 it('renders the writing recorder', () => {
   const div = document.createElement('div');
-  const {container, getByText} = render(<App />, div);
+  const {container} = render(<App />, div);
 
   const input = container.querySelector('input');
 
   expect(input).toBeInTheDocument();
   expect(input).toBeEmpty();
+});
+
+it('records writing', async () => {
+  const expectedWriting = getlorem.words(5);
+  const div = document.createElement('div');
+  const {container, getByText} = render(<App />, div);
+
+  let input = container.querySelector('input');
+  input.textContent = expectedWriting;
+
+  input = await waitForElement(() =>
+    getByText(expectedWriting)
+  );
 });
