5a721afac1c427fdaa8c31ac03a5457dc07b751c
‘Nathan <ncash13@hotmail.com>
2018-10-25 11:28:02 -0500

    actually running tests now

--- pencil.py
+++ pencil.py
@@ -1,4 +1,7 @@
 class Pencil(object):
   def __init__(self, initPoint=100, initEraser=100):
+    if type(initPoint) is not int or type(initEraser) is not int or initPoint <= 0 or initEraser <= 0:
+      raise TypeError('Pencil point and eraser must be a positive integer.')
     self.point = initPoint
     self.eraser = initEraser
+    self.initialStats = {'point': initPoint, 'eraser': initEraser}
--- pencil.test.py
+++ pencil.test.py
@@ -4,10 +4,12 @@ from pencil import Pencil
 class TestPencil(unittest.TestCase):
   def setUp(self):
     pass
-  def makePencil(self):
+  def testMakePencil(self):
     pencil = Pencil()
     self.assertEqual(pencil.point, 100)
     self.assertEqual(pencil.eraser, 100)
+    with self.assertRaises(TypeError):
+      Pencil(0)
 
 if __name__ == '__main__':
   unittest.main()
