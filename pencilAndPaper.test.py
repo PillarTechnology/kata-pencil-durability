99f572b5e45c478dc709d0eba15d1ddf995ddca2
‘Nathan <ncash13@hotmail.com>
2018-10-25 11:35:15 -0500

    renamed files and class objects to better reflect functionality

--- pencilAndPaper.test.py
+++ pencilAndPaper.test.py
@@ -1,5 +1,5 @@
 import unittest
-from pencil import Pencil
+from pencilAndPaper import PencilAndPaper as Pencil
 
 class TestPencil(unittest.TestCase):
   def setUp(self):
@@ -10,6 +10,10 @@ class TestPencil(unittest.TestCase):
     self.assertEqual(pencil.eraser, 100)
     with self.assertRaises(TypeError):
       Pencil(0)
+  def testWrite(self):
+    pencil = Pencil(10)
+    self.assertEqual(pencil.point, 10)
+    
 
 if __name__ == '__main__':
   unittest.main()
