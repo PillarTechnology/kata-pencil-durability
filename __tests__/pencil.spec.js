bf4980280bda9ab372c88776f280b9498e4f48dc
Derek Parson <parson.derek@gmail.com>
2021-05-18 17:53:21 -0400

    Add sharpen tests, refactor original pencil tests to reflect default values now added to pencil class

--- __tests__/pencil.spec.js
+++ __tests__/pencil.spec.js
@@ -10,21 +10,21 @@ describe("Pencil class", () => {
   });
   test("Pencil constructor should create a new pencil object with default properties if nothing passed in", () => {
     const pencil = new Pencil();
-    expect(pencil.point).toBe(0);
-    expect(pencil.size).toBe(0);
-    expect(pencil.eraser).toBe(0);
+    expect(pencil.point).toBe(50);
+    expect(pencil.size).toBe(10);
+    expect(pencil.eraser).toBe(50);
   });
   test("Pencil constructor should create a new pencil object with point property equal to value passed in", () => {
     const pencil = new Pencil(1);
     expect(pencil.point).toBe(1);
-    expect(pencil.size).toBe(0);
-    expect(pencil.eraser).toBe(0);
+    expect(pencil.size).toBe(10);
+    expect(pencil.eraser).toBe(50);
   });
   test("Pencil constructor should create a new pencil object with size property equal to value passed in", () => {
-    const pencil = new Pencil(10, 10);
+    const pencil = new Pencil(10, 20);
     expect(pencil.point).toBe(10);
-    expect(pencil.size).toBe(10);
-    expect(pencil.eraser).toBe(0);
+    expect(pencil.size).toBe(20);
+    expect(pencil.eraser).toBe(50);
   });
 });
 
@@ -42,6 +42,23 @@ describe("Write method", () => {
     test("it should cutoff text if pencil point not durable enough", () => {
       pencil.write("and very rewarding!", blankPaper);
       expect(blankPaper.text).toBe("TDD is fun and very re");
+    });
+  });
+});
+
+describe("Sharpen method", () => {
+  describe("Pencil point dulls after writing", () => {
+    const pencil = new Pencil(20);
+    const mantra = "TDD is fun "; // counts 11 characters, 6 for capital, 5 for lowercase
+    const blankPaper = new Paper();
+    pencil.write(mantra, blankPaper);
+    
+    test("it should reduce pencil point", () => {
+      expect(pencil.point).toBe(9);
+    });
+    test("it should sharpen pencil point back to original point", () => {
+      pencil.sharpen();
+      expect(pencil.point).toBe(20);
     });
   });
 });
