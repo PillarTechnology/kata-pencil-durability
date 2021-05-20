bd3bd2afb4c846f9300db87d46c562d933a6bf0f
Derek Parson <parson.derek@gmail.com>
2021-05-19 22:33:45 -0400

    Add new tests for paper class to ensure erased property is updating when erase method is being called, comment out some pencil tests to work on tomorrow

--- __tests__/paper.spec.js
+++ __tests__/paper.spec.js
@@ -1,4 +1,5 @@
-const Paper = require("../kata/paper")
+const Paper = require("../kata/paper");
+const Pencil = require("../kata/pencil");
 
 describe("Paper class", () => {
   test("Paper constructor should create a new paper object text property with passed in string", () => {
@@ -8,5 +9,26 @@ describe("Paper class", () => {
   test("Paper constructor should create a new paper object text property with empty string", () => {
     const paper = new Paper();
     expect(paper.text).toBe("");
+  });
+  test("Paper constructor should create a new paper object with property erased set to false", () => {
+    const paper = new Paper();
+    expect(paper.erased).toBeFalsy();
+  });
+  test("Paper erased property should change from false and reflect a number when eraser method is called", () => {
+    const pencil = new Pencil();
+    const erase = "TDD is fun but perfecting TDD is more fun!"; // counts 11 characters, 6 for capital, 5 for lowercase
+    const blankPaper = new Paper();
+    pencil.write(erase, blankPaper);
+    pencil.erase("fun", blankPaper);
+    expect(blankPaper.erased).toBe(38);
+  });
+  test("Paper erased property should change from last erased index to reflect index of new erased word", () => {
+    const pencil = new Pencil();
+    const erase = "TDD is fun but perfecting TDD is more fun!"; // counts 11 characters, 6 for capital, 5 for lowercase
+    const blankPaper = new Paper();
+    pencil.write(erase, blankPaper);
+    pencil.erase("fun", blankPaper);
+    pencil.erase("TDD", blankPaper);
+    expect(blankPaper.erased).toBe(26);
   });
 });
--- __tests__/pencil.spec.js
+++ __tests__/pencil.spec.js
@@ -121,4 +121,19 @@ describe("Edit method", () => {
       expect(pencil.point).toBe(11);
     });
   });
+  // describe("Pencil will stop editing if runs out of pencil point", () => {
+  //   const pencil = new Pencil();
+  //   const mantra = "An apple a day keeps the doctor away"; // 30 characters, 1 capital 28 lowercase
+  //   const blankPaper = new Paper();
+  //   pencil.write(mantra, blankPaper);
+  //   pencil.erase("apple", blankPaper); // Should take away 5 from eraser
+  //   pencil.edit("artichoke", blankPaper); // Should take away 9 from pencil point
+
+  //   test("it should add 'artichoke' to blankPaper in space left from erasing 'apple' previously but run out of space and add '@' when appropriate", () => {
+  //     expect(blankPaper.text).toBe("An artich@k@ay keeps the doctor away");
+  //   });
+  //   test("it should degrade pencil point by correct amount when adding 'artich@k@' to paper", () => {
+  //     expect(pencil.point).toBe(11);
+  //   });
+  // });
 });
